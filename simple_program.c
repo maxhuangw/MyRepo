4c39f57a52a3c61af85620411b8de40a4890e4cc
maxhuangw <60605max@gmail.com>
2024-07-26 15:43:36 +0800

    fix: in universe

--- simple_program.c
+++ simple_program.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv) {
                 break;
             
             case 'j':
-                puts("Jerry is the most handsome professor in the world!\n");
+                puts("Jerry is the most handsome professor in the universe!\n");
                 break;
 	}
     }
